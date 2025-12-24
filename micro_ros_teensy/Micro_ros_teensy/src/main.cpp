b1ec2912600861795f403cef7f35e2c479fa65bf
Brinex <omjagtap300@gmail.com>
2025-12-24 20:47:15 +0530

    added chassis and arm micro ros

---
+++ micro_ros_teensy/Micro_ros_teensy/src/main.cpp
@@ -0,0 +1,157 @@
+#include <Arduino.h>
+#include <micro_ros_platformio.h>
+
+#include <Cytron.h>
+#include <Encoder.h>
+
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/int32.h>
+#include <irc_interfaces/msg/ps4.h>
+#include <geometry_msgs/msg/twist.h>
+
+#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
+#error This example is only avaliable for Arduino framework with serial transport.
+#endif
+
+int Right_front_motor_pin=8;
+int Right_front_dir_pin=9;
+
+int Left_front_motor_pin=0;
+int Left_front_dir_pin=1; 
+
+int Right_middle_motor_pin=10;
+int Right_middle_dir_pin=11;
+
+int Left_middle_motor_pin=2;
+int Left_middle_dir_pin=3;
+
+int Right_back_motor_pin=22;
+int Right_back_dir_pin=23;
+
+int Left_back_motor_pin=5;
+int Left_back_dir_pin=4;
+
+Cytron R_F( Right_front_motor_pin, Right_front_dir_pin, true);
+Cytron L_F( Left_front_motor_pin, Left_front_dir_pin, true);
+
+Cytron R_M( Right_middle_motor_pin, Right_middle_dir_pin, true);
+Cytron L_M( Left_middle_motor_pin, Left_middle_dir_pin, false);
+
+Cytron R_B( Right_back_motor_pin, Right_back_dir_pin, true);
+Cytron L_B( Left_back_motor_pin, Left_back_dir_pin, true);
+
+rcl_publisher_t publisher;
+rcl_subscription_t subscription;
+
+irc_interfaces__msg__Ps4 ps4_msg;
+geometry_msgs__msg__Twist twist_msg;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+float_t linear_x = 0.0;
+float_t angular_z = 0.0;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+// Error handle loop
+void error_loop() {
+  while(1) {
+    delay(100);
+  }
+}
+
+void subscription_callback(const void * msgin)
+{
+  if (msgin == NULL) {
+    return;
+  }
+
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *) msgin;
+
+  twist_msg = *msg; 
+  
+  linear_x = twist_msg.linear.x;
+  angular_z = twist_msg.angular.z;
+
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    float_t right_speed = linear_x + angular_z;
+    float_t left_speed = linear_x - angular_z;
+    
+    R_F.rotate(right_speed);
+    R_M.rotate(right_speed);
+    R_B.rotate(right_speed);
+
+    L_F.rotate(left_speed);
+    L_M.rotate(left_speed);
+    L_B.rotate(left_speed);
+
+  }
+}
+
+void setup() {
+  // Configure serial transport
+  Serial.begin(115200);
+  set_microros_serial_transports(Serial);
+  delay(2000);
+
+  pinMode(2, OUTPUT);
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(irc_interfaces, msg, Ps4),
+    "micro_ros_platformio_node_publisher"));
+
+  // create subscription
+  RCCHECK(rclc_subscription_init_default(
+    &subscription,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "/cmd_vel"));
+
+  // create timer,
+  const unsigned int timer_timeout = 0.05;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+    
+    // create executor
+    RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
+    RCCHECK(rclc_executor_add_subscription(
+      &executor,
+      &subscription,
+      &twist_msg,
+      &subscription_callback,
+      ON_NEW_DATA));
+
+}
+
+void loop() {
+  // delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(1)));
+}
---
+++ micro_ros_teensy/Micro_ros_teensy_Arm/src/main.cpp
@@ -0,0 +1,227 @@
+#include <Arduino.h>
+#include <micro_ros_platformio.h>
+
+#include <Cytron.h>
+#include <Encoder.h>
+
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+// #include <std_msgs/msg/int8_multi_array.h>
+#include <std_msgs/msg/float32_multi_array.h>
+#include <std_msgs/msg/int64_multi_array.h>
+
+#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
+#error This example is only avaliable for Arduino framework with serial transport.
+#endif
+
+int Turret_motor_pin=0;
+int Turret_dir_pin=1;
+
+int Shoulder_motor_pin=2;
+int Shoulder_dir_pin=3;
+
+int Elbow_motor_pin=7;
+int Elbow_dir_pin=6;
+
+int Wrist_pitch_motor_pin=5;
+int Wrist_pitch_dir_pin=4;
+
+int Wrist_roll_motor_pin=8;
+int Wrist_roll_dir_pin=9;
+
+Cytron Turret( Turret_motor_pin, Turret_dir_pin, true);
+
+Cytron Shoulder( Shoulder_motor_pin, Shoulder_dir_pin, true);
+
+Cytron Elbow( Elbow_motor_pin, Elbow_dir_pin, true);
+
+Cytron Wrist_pitch( Wrist_pitch_motor_pin, Wrist_pitch_dir_pin, false);
+
+Cytron Wrist_roll( Wrist_roll_motor_pin, Wrist_roll_dir_pin, true);
+
+Encoder Turret_encoder(30,31);
+Encoder Shoulder_encoder(34,35);
+Encoder Elbow_encoder(38,39);
+Encoder Wrist_pitch_encoder(41,40);
+Encoder Wrist_roll_encoder(26,27);
+
+
+rcl_publisher_t publisher;
+rcl_publisher_t pub;
+rcl_subscription_t subscription;
+
+std_msgs__msg__Float32MultiArray joint_vel;
+std_msgs__msg__Int64MultiArray encoder_data;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+int64_t encoder_buffer[5];
+float joint_vel_buffer[5];
+
+volatile bool got_cmd = false;
+
+int error1;
+int error2;
+
+int prev1 = 0;
+int prev2 = 0;
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+// Error handle loop
+void error_loop() {
+  digitalWrite(13, HIGH);
+  delay(1000);
+}
+
+void subscription_callback(const void * msgin)
+{
+  if (msgin == NULL) {
+    return;
+  }
+
+  const std_msgs__msg__Float32MultiArray * msg = (const std_msgs__msg__Float32MultiArray *) msgin;
+
+  for (size_t i = 0; i < 5; i++) {
+    joint_vel.data.data[i] = msg->data.data[i];
+  }
+
+  got_cmd = true;
+  
+  // Set motor speeds
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    
+    encoder_data.data.data[0] = Elbow_encoder.read();
+    encoder_data.data.data[1] = Shoulder_encoder.read();
+    encoder_data.data.data[2] = Turret_encoder.read();
+    encoder_data.data.data[3] = Wrist_pitch_encoder.read();
+    encoder_data.data.data[4] = Wrist_roll_encoder.read();
+    
+    RCSOFTCHECK(rcl_publish(&publisher, &encoder_data, NULL));
+    RCSOFTCHECK(rcl_publish(&pub, &joint_vel, NULL));
+    
+  }
+}
+
+void setup() {
+  // Configure serial transport
+  Serial.begin(115200);
+  set_microros_serial_transports(Serial);
+  delay(2000);
+  
+  while (error1 != 0 ) {
+    
+    Shoulder.rotate(-255);
+    
+    delay(500);
+    
+    float Shoulder = (Shoulder_encoder.read() * (360.0 / 8192.0));
+    
+    error1 = Shoulder - prev1;
+    
+    prev1 = Shoulder;
+  } 
+  
+  Shoulder.rotate(0);
+  
+  while (error2 != 0 ) {
+    
+    Elbow.rotate(50);
+    delay(500);
+    
+    float Elbow = (Elbow_encoder.read() * (360.0 / 8192.0));
+    
+    error2 = Elbow - prev2;
+    
+    prev2 = Elbow;
+    
+  }
+  Elbow.rotate(0);
+  
+  Shoulder_encoder.write(0);
+  Elbow_encoder.write(0);
+  
+  // digitalWrite(13,HIGH);
+  
+  encoder_data.data.data = encoder_buffer;
+  encoder_data.data.size = 5;
+  encoder_data.data.capacity = 5;
+
+  joint_vel.data.data = joint_vel_buffer;
+  joint_vel.data.size = 5;
+  joint_vel.data.capacity = 5;
+
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int64MultiArray),
+    "/arm_encoders"));
+
+  RCCHECK(rclc_publisher_init_default(
+    &pub,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
+    "/joint_data"));
+
+  // create subscription
+  RCCHECK(rclc_subscription_init_default(
+    &subscription,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32MultiArray),
+    "/joints_command"));
+
+  // create timer,
+  const unsigned int timer_timeout = 50;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+    
+    // create executor
+    RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+    RCCHECK(rclc_executor_add_timer(&executor, &timer));
+    RCCHECK(rclc_executor_add_subscription(
+      &executor,
+      &subscription,
+      &joint_vel,
+      &subscription_callback,
+      ON_NEW_DATA));
+
+}
+
+void loop() {
+  // delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
+
+  if (got_cmd) {
+    Elbow.rotate(joint_vel.data.data[0]);
+    Shoulder.rotate(joint_vel.data.data[1]);
+    Turret.rotate(joint_vel.data.data[2]);
+    Wrist_pitch.rotate(joint_vel.data.data[3]);
+    // Wrist_roll.rotate(joint_vel.data.data[4]);
+  }
+
+}
